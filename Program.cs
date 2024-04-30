e9986c05b9fdce73e3e491929905d558e96ea415
r8vnhill <reachme@ravenhill.cl>
2024-04-30 18:35:35 -0400

    Add PDF viewer, implement notes page, and remove counter page
    This commit introduces a PDF viewer feature and adds a new Notes page. It also removes the previously used Counter page, alongside minor corrections to other pages to improve the overall functionality of the site. Syntax cleaning was done in CourseSection.cs for better readability and SyncFusion Blazor package was added to support implementation of the PDF viewer.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ravenhill_blazor;
 using Ravenhill.Data;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ThemeService>();
+builder.Services.AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+async function loadPDF(pdfPath) {
+    const pdf = await pdfjsLib.getDocument(pdfPath).promise;
+    const page = await pdf.getPage(1); // Get the first page
+    const scale = 1.5; // Render scale
+    const viewport = page.getViewport({ scale: scale });
+
+    const canvas = document.getElementById('pdf-canvas');
+    const context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderContext = {
+        canvasContext: context,
+        viewport: viewport
+    };
+    await page.render(renderContext).promise;
+}
